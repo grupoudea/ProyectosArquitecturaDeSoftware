1d50989c34b72c1190b396dbfeb9a5fec0d06da8
kellytbn <kellytbn14@gmail.com>
2021-01-23 16:44:12 -0500

    Se crean repositories del módulo Cliente

---
+++ Desarrollos/RTF3/saetel-backend/src/main/java/co/udea/saetelbackend/repository/clientes/EstratoRepository.java
@@ -0,0 +1,7 @@
+package co.udea.saetelbackend.repository.clientes;
+
+import co.udea.saetelbackend.repository.clientes.entity.Estrato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstratoRepository extends JpaRepository<Estrato, Integer> {
+}
