8ec2f1149769d30d5e84e2153ae63c033794e65b
kellytbn <kellytbn14@gmail.com>
2021-01-23 16:46:40 -0500

    Se crean repositories del módulo Archivo

---
+++ Desarrollos/RTF3/saetel-backend/src/main/java/co/udea/saetelbackend/repository/archivos/PeriodoCargaArchivoRepository.java
@@ -0,0 +1,7 @@
+package co.udea.saetelbackend.repository.archivos;
+
+import co.udea.saetelbackend.repository.archivos.entity.PeriodoCargaArchivo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PeriodoCargaArchivoRepository extends JpaRepository<PeriodoCargaArchivo, Integer> {
+}
