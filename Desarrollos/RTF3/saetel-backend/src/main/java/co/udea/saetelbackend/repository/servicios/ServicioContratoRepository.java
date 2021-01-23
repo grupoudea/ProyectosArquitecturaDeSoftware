3cb49e63d4b9cbc534bc82ca3b59db1613e31b1e
kellytbn <kellytbn14@gmail.com>
2021-01-23 16:40:18 -0500

    Se crean repositories del módulo Servicio

---
+++ Desarrollos/RTF3/saetel-backend/src/main/java/co/udea/saetelbackend/repository/servicios/ServicioContratoRepository.java
@@ -0,0 +1,7 @@
+package co.udea.saetelbackend.repository.servicios;
+
+import co.udea.saetelbackend.repository.servicios.entity.ServicioContrato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicioContratoRepository extends JpaRepository<ServicioContrato, Integer> {
+}
