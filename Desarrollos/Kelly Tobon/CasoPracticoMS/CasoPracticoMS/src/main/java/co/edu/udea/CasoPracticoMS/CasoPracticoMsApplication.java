2e95ab497b6c53a93a5223a53b218b25c9ba5388
kellytbn <kellytbn14@gmail.com>
2021-01-23 10:53:35 -0500

    Se crea el proyecto

---
+++ Desarrollos/Kelly Tobon/CasoPracticoMS/CasoPracticoMS/src/main/java/co/edu/udea/CasoPracticoMS/CasoPracticoMsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.CasoPracticoMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasoPracticoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasoPracticoMsApplication.class, args);
+	}
+
+}
---
+++ Desarrollos/Kelly Tobon/CasoPracticoMS/CasoPracticoMS/src/test/java/co/edu/udea/CasoPracticoMS/CasoPracticoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udea.CasoPracticoMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoPracticoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
