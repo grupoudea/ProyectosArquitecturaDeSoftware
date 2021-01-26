69261faff533c39b3c25147e007c926fc73276ca
Oscar Darío Botero Vargas <odbotero@gmail.com>
2021-01-25 20:02:34 -0500

    Ediciones menores que no alteran el funcionamiento del programa...

--- Desarrollos/RTF3/saetel-backend/src/main/java/co/udea/saetelbackend/SaetelBackendApplication.java
+++ Desarrollos/RTF3/saetel-backend/src/main/java/co/udea/saetelbackend/SaetelBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SaetelBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SaetelBackendApplication.class, args);
     }
-
 }
