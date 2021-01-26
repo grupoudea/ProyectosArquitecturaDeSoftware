09a019f2c10f24efc44907c61ff4285255ed7d80
kellytbn <kellytbn14@gmail.com>
2021-01-25 20:22:37 -0500

    Se agregan services para crear cliente y contrato

--- Desarrollos/RTF3/saetel-backend/src/main/java/co/udea/saetelbackend/services/clientes/ContratoService.java
+++ Desarrollos/RTF3/saetel-backend/src/main/java/co/udea/saetelbackend/services/clientes/ContratoService.java
@@ -1,8 +1,13 @@
 package co.udea.saetelbackend.services.clientes;
 
 import co.udea.saetelbackend.repository.clientes.ContratoRepository;
+import co.udea.saetelbackend.repository.clientes.entity.Contrato;
+import co.udea.saetelbackend.util.exception.DataDuplicatedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,4 +19,13 @@ public class ContratoService {
         this.repository = repository;
     }
 
+    public Contrato create(Contrato contrato){
+        if(Objects.nonNull(contrato.getId())){
+            Optional<Contrato> contratoOptional = repository.findById(contrato.getId());
+            if(contratoOptional.isPresent()){
+                throw new DataDuplicatedException("Contrato duplicado");
+            }
+        }
+        return repository.save(contrato);
+    }
 }
