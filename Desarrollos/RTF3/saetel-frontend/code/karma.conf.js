8080063046bb4a46d2908f00ba28fb0423f5213c
kellytbn <kellytbn14@gmail.com>
2021-01-28 21:58:05 -0500

    Se inicia el proyecto

--- Desarrollos/RTF3/saetel-frontend/code/karma.conf.js
+++ Desarrollos/RTF3/saetel-frontend/code/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/saetel-frontend'),
+      dir: require('path').join(__dirname, './coverage/code'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Desarrollos/RTF3/saetel-frontend/code/src/app/app.component.spec.ts
+++ Desarrollos/RTF3/saetel-frontend/code/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'saetel-frontend'`, () => {
+  it(`should have as title 'code'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('saetel-frontend');
+    expect(app.title).toEqual('code');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('saetel-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('code app is running!');
   });
 });
