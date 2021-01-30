5145bac7e75f3ed80ee1115551a0b17eb23459f0
kellytbn <kellytbn14@gmail.com>
2021-01-29 22:22:06 -0500

    Se crean servicios para listar y crear

--- Desarrollos/RTF3/saetel-backend/src/main/java/co/udea/saetelbackend/services/servicios/ServicioService.java
+++ Desarrollos/RTF3/saetel-backend/src/main/java/co/udea/saetelbackend/services/servicios/ServicioService.java
@@ -1,16 +1,32 @@
 package co.udea.saetelbackend.services.servicios;
 
 import co.udea.saetelbackend.repository.servicios.ServicioRepository;
+import co.udea.saetelbackend.repository.servicios.entity.Servicio;
+import co.udea.saetelbackend.util.exception.DataDuplicatedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class ServicioService {
 
-    private ServicioRepository repository;
+    private final ServicioRepository repository;
 
     public ServicioService(ServicioRepository repository) {
         this.repository = repository;
+    }
+
+    public Servicio create(Servicio servicio){
+        if(Objects.nonNull(servicio.getId())){
+            Optional<Servicio> servicioOptional = repository.findById(servicio.getId());
+            if(servicioOptional.isPresent()){
+                throw new DataDuplicatedException("Servicio duplicado");
+            }
+        }
+        return repository.save(servicio);
     }
 }
