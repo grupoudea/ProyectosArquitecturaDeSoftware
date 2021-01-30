e7fc343be045ddfd0363ca978d6dfce66d6f8260
Juan Guillermo Hernandez Alarcon <juanguillermoalarcon@gmail.com>
2021-01-30 00:43:53 -0500

    se agrega funcionalidad de descargar plantilla

--- Desarrollos/RTF3/saetel-frontend/code/src/app/empresas/importacion-archivo/importacion-archivo.component.ts
+++ Desarrollos/RTF3/saetel-frontend/code/src/app/empresas/importacion-archivo/importacion-archivo.component.ts
@@ -15,99 +15,107 @@ export class ImportacionArchivoComponent implements OnInit {
 
   flag: boolean;
   arrayBuffer: any;
-  jsontext: any[]= [];
+  jsontext: any[] = [];
   disabledBtnGuardarArchivo = true;
-  
+
   constructor() { }
 
   ngOnInit(): void {
     this.items = [
-      {label: 'Home', icon: 'pi pi-fw pi-home', routerLink: ['../../empresas']},
-      {label: 'Importar archivo', icon: 'pi pi-fw pi-upload', routerLink: ['../importar-archivo']},
+      { label: 'Home', icon: 'pi pi-fw pi-home', routerLink: ['../../empresas'] },
+      { label: 'Importar archivo', icon: 'pi pi-fw pi-upload', routerLink: ['../importar-archivo'] },
     ];
     this.activeItem = this.items[0];
   }
 
-  public onUpload(event: {[x: string]: any[] }){
+  public onUpload(event: { [x: string]: any[] }) {
     this.excelToJson(event['files'][0])
   }
 
-  public onRemove(event: {[x: string]: any[] }){
+  public onRemove(event: { [x: string]: any[] }) {
 
   }
 
-  descargarPlantilla(){}
+  descargarPlantilla() {
+    const nombreArchivo = "PLANTILLA_CLIENTES.xlsx"
+    const rutaArchivo = "assets/PLANTILLA_CLIENTES.xlsx"
+    const linkDescarga = document.createElement('a');
+    linkDescarga.href = rutaArchivo
+    linkDescarga.setAttribute('download', nombreArchivo)
+    document.body.appendChild(linkDescarga)
+    linkDescarga.click()
+  }
 
-  public excelToJson(file: File){
+  public excelToJson(file: File) {
     this.flag = false;
     let fileReader = new FileReader();
     fileReader.readAsArrayBuffer(file);
     fileReader.onload = (e) => {
-        this.arrayBuffer = fileReader.result;
-        var data = new Uint8Array(this.arrayBuffer);
-        var arr = new Array();
-        for(var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
-        var bstr = arr.join("");
-        var workbook = XLSX.read(bstr, {type:"binary"});
-        var first_sheet_name = workbook.SheetNames[0];
-        var worksheet = workbook.Sheets[first_sheet_name];
-        this.jsontext = XLSX.utils.sheet_to_json<string>(worksheet,{raw:true});     
-        console.log(this.jsontext);
-           
-        this.validarJson(this.jsontext);
+      this.arrayBuffer = fileReader.result;
+      var data = new Uint8Array(this.arrayBuffer);
+      var arr = new Array();
+      for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+      var bstr = arr.join("");
+      var workbook = XLSX.read(bstr, { type: "binary" });
+      var first_sheet_name = workbook.SheetNames[0];
+      var worksheet = workbook.Sheets[first_sheet_name];
+      this.jsontext = XLSX.utils.sheet_to_json<string>(worksheet, { raw: true });
+      console.log(this.jsontext);
+
+      this.validarJson(this.jsontext);
     }
-    fileReader.onloadend = (e) =>{
-        this.flag = true;
+    fileReader.onloadend = (e) => {
+      this.flag = true;
     }
   }
 
-  validarJson(jsontext: any[]){
-    if(this.jsontext.length>0){
+  validarJson(jsontext: any[]) {
+    if (this.jsontext.length > 0) {
       let json = jsontext[0];
-      const obj = JSON.parse(JSON.stringify(json));    
+      const obj = JSON.parse(JSON.stringify(json));
       let arrColumnas = this.obtenerColumnasPlantilla();
       let jsonValido = true;
-      for(const i in obj){
-        let dato = arrColumnas.find(item => item===i);
-        if(!dato){
+      for (const i in obj) {
+        let dato = arrColumnas.find(item => item === i);
+        if (!dato) {
           this.disabledBtnGuardarArchivo = true;
           jsonValido = false;
           console.log("El archivo no cumple con los requerimientos de la plantilla");
-          
+
           // this.mensajeToast('error', `El archivo no cumple con los requerimientos de la plantilla`, false)
           break;
         }
       }
-      if(jsonValido){
+      if (jsonValido) {
         this.disabledBtnGuardarArchivo = false;
       }
-    }else{
+    } else {
       console.log("El archivo no tiene registros");
-      
+
       // this.mensajeToast('error', `El archivo no tiene registros`, false)
       this.disabledBtnGuardarArchivo = true;
     }
   }
 
-  obtenerColumnasPlantilla(){
-    const obj2 = JSON.parse(JSON.stringify(new ColumnasPlantillaExcel())); 
+  obtenerColumnasPlantilla() {
+    const obj2 = JSON.parse(JSON.stringify(new ColumnasPlantillaExcel()));
     let arr: any[] = [];
-    for(const i in obj2){
+    for (const i in obj2) {
       arr.push(i);
     }
     return arr;
   }
 
-  construirClientes(){
+  construirClientes() {
     this.disabledBtnGuardarArchivo = true;
     const clientes: any[] = [];
     let huboError = false;
-    for(let registro of this.jsontext){
+    for (let registro of this.jsontext) {
       registro = this.limpiarEspacios(registro);
     }
   }
 
-  limpiarEspacios(registro: any){
+  limpiarEspacios(registro: any) {
     let registroLimpio: any = {};
     registroLimpio.TIPO_DOCUMENTO = this.getTextoLimpio(registro.TIPO_DOCUMENTO);
     registroLimpio.NUMERO_DOCUMENTO = this.getTextoLimpio(registro.NUMERO_DOCUMENTO);
@@ -126,8 +134,8 @@ export class ImportacionArchivoComponent implements OnInit {
     registroLimpio.ESTRATO = this.getTextoLimpio(registro.ESTRATO);
   }
 
-  getTextoLimpio(valor: any){
-    return valor?valor.toString().trim():null;
+  getTextoLimpio(valor: any) {
+    return valor ? valor.toString().trim() : null;
   }
 
 }
